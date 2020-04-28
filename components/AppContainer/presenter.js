6e108dd8c676ebc18d77f06ab947e21d38bc1ea5
junngo <myeongjun.ko@gmail.com>
2020-04-28 23:49:39 +0900

    added AuthNavigation

--- components/AppContainer/presenter.js
+++ components/AppContainer/presenter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { View, Text, StatusBar, StyleSheet } from "react-native";
+import AuthNavigation from "../../navigation/AuthNavigation";
 
 class AppContainer extends Component {
   static propTypes = {
@@ -11,25 +12,16 @@ class AppContainer extends Component {
     const { isLoggedIn } = this.props;
 
     return (
-      <View style={styles.container}>
+      <>
         <StatusBar hidden={false} />
         {isLoggedIn ? (
           <Text>You are logged in</Text>
-        ) : (
-          <Text>I dont know you</Text>
+            ) : (
+          <AuthNavigation />
         )}
-      </View>
+      </>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
 
 export default AppContainer;
---
+++ constants.js
@@ -0,0 +1,3 @@
+// export const API_URL = "http://10.0.2.2:8000";
+export const API_URL = "http://127.0.0.1:8000";
+export const FB_APP_ID = "178190336837752";
